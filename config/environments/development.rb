8d6cc242ecab56e682d02030c6344a8337faa9e6
mnNadeem <muhammad.nadeem@devsinc.com>
2022-07-07 06:11:35 -0300

    Enable Production Email

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -60,11 +60,19 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
-  # ActionMailer Config
-  config.action_mailer.default_url_options = { host: 'localhost:3000' }
+  config.action_mailer.default_url_options = {
+    host: 'localhost',
+    port: 3000
+  }
+  config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :smtp
-  # change to true to allow email to be sent during development
-  config.action_mailer.perform_deliveries = false
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default charset: 'utf-8'
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: 587,
+    domain: 'example.com',
+    user_name: 'muhammad.nadeem@devsinc.com',
+    password: 'nubpelxmhcqqpyzm',
+    authentication: plain,
+    enable_starttls_auto: true
+  }
 end
